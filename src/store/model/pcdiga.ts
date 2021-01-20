b0a3d0625f0b9b7ede4118ff6003056173c50892
Fabio Carneiro <fabio.carneiro@outlook.com>
2021-01-20 07:46:03 +0000

    Add: PCdiga store

---
+++ src/store/model/pcdiga.ts
@@ -0,0 +1,164 @@
+import {Store} from './store';
+
+export const PCDiga: Store = {
+  currency: '€',
+  labels: {
+    inStock: {
+      container: '#product-addtocart-button > span',
+      text: ['Adicionar ao carrinho'],
+    },
+    maxPrice: {
+      container: '.price-wrapper  > span',
+      euroFormat: true,
+    },
+    outOfStock: [
+      {
+        container: '#skrey_estimate_date_product_page_wrapper > span',
+        text: ['Sem stock'],
+      },
+    ],
+  },
+  links: [
+    {
+      brand: 'test:brand',
+      model: 'test:model',
+      series: 'test:series',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-msi-geforce-rtx-3080-suprim-x-10g-912-v389-006',
+    },
+    {
+      brand: 'asus',
+      model: 'strix oc',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-asus-rog-strix-geforce-rtx-3080-10gb-gddr6x-oc-edition-90yv0fa1-m0nm00',
+    },
+    {
+      brand: 'asus',
+      model: 'tuf',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-asus-tuf-gaming-geforce-rtx-3080-10gb-gddr6x-90yv0fb0-m0nm00',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus xtreme',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-gigabyte-geforce-rtx-3080-aorus-xtreme-10gb-gddr6x-gv-n3080aorus-x-10gd',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'eagle oc',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-gigabyte-geforce-rtx-3080-eagle-10gb-gddr6x-oc-edition-gvn3080eo-00-10',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'gaming oc',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-gigabyte-geforce-rtx-3080-gaming-10gb-gddr6x-oc-edition-gvn3080go-00-10',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'vision oc',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-gigabyte-geforce-rtx-3080-vision-10gb-gddr6x-oc-edition-gv-n3080vision-oc-10',
+    },
+    {
+      brand: 'msi',
+      model: 'gaming x trio',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-msi-geforce-rtx-3080-gaming-x-trio-10g-912-v389-005',
+    },
+    {
+      brand: 'msi',
+      model: 'ventus 3x',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-msi-geforce-rtx-3080-ventus-3x-10g-oc-912-v389-001',
+    },
+    {
+      brand: 'zotac',
+      model: 'amp holo',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-zotac-gaming-geforce-rtx-3080-10gb-gddr6x-amp-holo-zt-a30800f-10p',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus xtreme',
+      series: '3080',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-gigabyte-geforce-rtx-3080-aorus-xtreme-10gb-gddr6x-gv-n3080aorus-x-10gd',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'eagle oc',
+      series: '3060ti',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-gigabyte-geforce-rtx-3060-ti-eagle-oc-8gb-gddr6-gvn306teo-00-10',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'aorus master',
+      series: '3060ti',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-gigabyte-geforce-rtx-3060-ti-aorus-master-8gb-gddr6-gvn306tam-00-10',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'gaming oc pro',
+      series: '3060ti',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-gigabyte-geforce-rtx-3060-ti-gaming-oc-pro-8gb-gddr6-gvn306tgop-00-10',
+    },
+    {
+      brand: 'gigabyte',
+      model: 'gaming oc',
+      series: '3060ti',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-gigabyte-geforce-rtx-3060-ti-gaming-oc-8gb-gddr6-gvn306tgo-00-10',
+    },
+    {
+      brand: 'msi',
+      model: 'ventus 2x',
+      series: '3060ti',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-msi-geforce-rtx-3060-ti-ventus-2x-8gb-gddr6-oc-912-v390-009',
+    },
+    {
+      brand: 'msi',
+      model: 'ventus 3x',
+      series: '3060ti',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-msi-geforce-rtx-3060-ti-ventus-3x-8gb-gddr6-oc-912-v397-001',
+    },
+    {
+      brand: 'zotac',
+      model: 'twin edge oc',
+      series: '3060ti',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-zotac-gaming-geforce-rtx-3060-ti-8gb-gddr6-twin-edge-oc-zt-a30610h-10m',
+    },
+    {
+      brand: 'zotac',
+      model: 'twin edge',
+      series: '3060ti',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-zotac-gaming-geforce-rtx-3060-ti-8gb-gddr6-twin-edge-zt-a30610e-10m',
+    },
+    {
+      brand: 'asus',
+      model: 'dual',
+      series: '3060ti',
+      url:
+        'https://www.pcdiga.com/componentes/placas-graficas/placas-graficas-nvidia/placa-grafica-asus-dual-geforce-rtx-3060-ti-8gb-gddr6-oc-90yv0g12-m0na00',
+    },
+  ],
+  name: 'pcdiga',
+};
